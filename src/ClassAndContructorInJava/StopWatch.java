b11709f970898198f19a7c49f223af2801f946dc
taitv1112 <91813628+taitv1112@users.noreply.github.com>
2021-11-06 20:12:26 +0700

    Xây dựng lớp StopWatch

---
+++ src/ClassAndContructorInJava/StopWatch.java
@@ -0,0 +1,42 @@
+package ClassAndContructorInJava;
+
+public class StopWatch {
+    private long startTime = 0;
+    private long stopTime = 0;
+    private boolean running = false;
+
+    public void start() {
+        this.startTime = System.currentTimeMillis();
+        this.running = true;
+    }
+
+
+    public void stop() {
+        this.stopTime = System.currentTimeMillis();
+        this.running = false;
+    }
+
+
+    ////thoi gian troi tinh bang mili giay
+    public long getElapsedTime() {
+        long elapsed;
+        if (running) {
+            elapsed = (System.currentTimeMillis() - startTime);
+        } else {
+            elapsed = (stopTime - startTime);
+        }
+        return elapsed;
+    }
+
+
+    //thoi gian troi tinh bang giay
+    public long getElapsedTimeSecs() {
+        long elapsed;
+        if (running) {
+            elapsed = ((System.currentTimeMillis() - startTime) / 1000);
+        } else {
+            elapsed = ((stopTime - startTime) / 1000);
+        }
+        return elapsed;
+    }
+}
