b4b1806f613e70cddd85fc5ea34cecf4e3fa8019
taitv1112 <91813628+taitv1112@users.noreply.github.com>
2021-11-12 00:14:24 +0700

    update MoveablePoint

--- src/PointAndMoveablePoint/MoveablePoint.java
+++ src/PointAndMoveablePoint/MoveablePoint.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 public class MoveablePoint extends Point{
     private float xSpeed =0.0f;
     private float ySpeed =0.0f;
-
     public MoveablePoint(float x, float y, float xSpeed, float ySpeed) {
         super(x, y);
         this.xSpeed = xSpeed;
@@ -50,8 +49,7 @@ public class MoveablePoint extends Point{
         return "(xS,yS)= "+Arrays.toString(getSpeed())+super.toString();
     }
     public MoveablePoint move(MoveablePoint moveablePoint){
-        moveablePoint.x += xSpeed; ;
-        moveablePoint.y += ySpeed;
+        moveablePoint.setXY(moveablePoint.getX() +xSpeed, moveablePoint.getY()+ySpeed);
         return this;
     }
 }
