22c9538cf43d33cb2d56688281a2c8f378803650
taitv1112 <91813628+taitv1112@users.noreply.github.com>
2021-11-12 00:19:12 +0700

    update MoveablePoint

--- src/PointAndMoveablePoint/test.java
+++ src/PointAndMoveablePoint/test.java
@@ -5,15 +5,17 @@ import java.util.Arrays;
 
 public class test {
     public static void main(String[] args) {
-        MoveablePoint moveablePoint = new MoveablePoint(6,8,5,5);
-        moveablePoint.move(moveablePoint);
-        System.out.println(moveablePoint.move(moveablePoint));
-        moveablePoint.setSpeed(20,30);
-        System.out.println(moveablePoint.move(moveablePoint));
-        Point point = new Point(10,10);
-        moveablePoint.getXY();
-        System.out.println(Arrays.toString(moveablePoint.getXY()));
-        System.out.println(moveablePoint.toString() );
+        MoveablePoint moveablePoint = new MoveablePoint(10,10,5,5);
+        System.out.println(moveablePoint.toString());
+        for (int i = 0; i < 5; i++) {
+            moveablePoint.move(moveablePoint);
+            System.out.println();
+            System.out.println(moveablePoint.move(moveablePoint));
+        }
+
+
+
+
 
     }
 }
