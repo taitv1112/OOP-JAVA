5d5bede907f95e1cbe02695fcb4a15c4276193ce
taitv1112 <91813628+taitv1112@users.noreply.github.com>
2021-11-16 15:14:47 +0700

    test

--- src/TestModule2/Controller.java
+++ src/TestModule2/Controller.java
@@ -9,6 +9,7 @@ public class Controller {
 
     public static Staff createStaff() {
         System.out.println("Nhập tên nhân viên: ");
+        sc.nextLine();
         String name = sc.nextLine();
         System.out.println("Nhập số điện thoại nhân viên: ");
         int phoneNumbers = sc.nextInt();
@@ -30,7 +31,7 @@ public class Controller {
         System.out.println("Nhập id nhân viên cần xóa");
         int id = sc.nextInt();
         int index = -1;
-        if (checkIdInStaff(id) > 0) {
+        if (checkIdInStaff(id) >= 0) {
             index = checkIdInStaff(id);
             Staff[] newStaff = new Staff[staffs.length - 1];
             System.arraycopy(staffs, 0, newStaff, 0, index);
@@ -50,30 +51,34 @@ public class Controller {
         }
         return 0;
     }
-    public static void showStaff(){
-        for (Staff nv:staffs) {
+
+    public static void showStaff() {
+        for (Staff nv : staffs) {
             System.out.println(nv.toString());
         }
     }
-    public static void editStaff(){
+
+    public static void editStaff() {
         System.out.println("Nhập id nhân viên cần sửa");
         int id = sc.nextInt();
         int index = -1;
-        if (checkIdInStaff(id) > 0) {
+        if (checkIdInStaff(id) >= 0) {
             index = checkIdInStaff(id);
             System.out.println("Nhập tên nhân viên: ");
+            sc.nextLine();
             String name = sc.nextLine();
             System.out.println("Nhập số điện thoại nhân viên: ");
             int phoneNumbers = sc.nextInt();
             System.out.println("Nhập địa chỉ của nhân viên: ");
             sc.nextLine();
             String address = sc.nextLine();
-            staffs[index].setAll(name,phoneNumbers,address);
+            staffs[index].setAll(name, phoneNumbers, address);
         } else {
             System.out.println("id: " + id + " bạn nhập không tồn tại.");
         }
     }
-    public static void menu(){
+
+    public static void menu() {
         while (true) {
             System.out.println("Menu: ");
             System.out.println("1.Thêm Nhân Viên ");
