931c6cb2e688c800bafa05d564261f934a89262c
taitv1112 <91813628+taitv1112@users.noreply.github.com>
2021-11-18 08:08:14 +0700

    [Optional] [Thực hành] Cài đặt cây tìm kiếm nhị phân

---
+++ src/Giat_Thuat/JavaColection/Binary_Search_Tree/AbstractTree.java
@@ -0,0 +1,7 @@
+package Giat_Thuat.JavaColection.Binary_Search_Tree;
+
+public abstract class AbstractTree <E> implements Tree<E> {
+    @Override /** Inorder traversal from the root*/
+    public void inorder() {
+    }
+}
---
+++ src/Giat_Thuat/JavaColection/Binary_Search_Tree/TreeNode.java
@@ -0,0 +1,12 @@
+package Giat_Thuat.JavaColection.Binary_Search_Tree;
+
+public class TreeNode <E>{
+    protected E element;
+    protected TreeNode<E> left;
+    protected TreeNode<E> right;
+
+    public TreeNode(E e) {
+        element = e;
+    }
+}
+
